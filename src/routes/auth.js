d48c79535ebfa218aeebade3728397737023c05a
xTheSnopx <xjuanesh@gmail.com>
2026-04-08 10:21:24 -0500

    BackEnd Inicial

---
+++ src/routes/auth.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const authController = require('../controllers/AuthController');
+
+// Auth Endpoints
+router.post('/register', (req, res) => authController.register(req, res));
+router.post('/login', (req, res) => authController.login(req, res));
+
+module.exports = router;
